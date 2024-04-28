bcb2e8fdc247dcae2696ec3fb1b1e362a926da74
dandansoy20 <158149528+dandansoy20@users.noreply.github.com>
2024-04-28 17:09:18 +0800

    student dashboard and login/signup

---
+++ kld-events/admin/pages/organizer.php
@@ -0,0 +1,143 @@
+<!--begin::Content-->
+<div class="content  d-flex flex-column flex-column-fluid" id="kt_content">
+
+
+    <!--begin::Entry-->
+    <div class="d-flex flex-column-fluid">
+        <!--begin::Container-->
+        <div class=" container ">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
+                    <!--begin::Card-->
+                    <div class="card card-custom gutter-b card-stretch">
+                        <!--begin::Body-->
+                        <div class="card-body pt-4">
+                            <!--begin::Toolbar-->
+                            <div class="d-flex justify-content-end">
+                                <div class="dropdown dropdown-inline" data-toggle="tooltip" title="Quick actions"
+                                    data-placement="left">
+                                    <a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="ki ki-bold-more-hor"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+                                        <!--begin::Navigation-->
+                                        <ul class="navi navi-hover py-5">
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-drop"></i></span>
+                                                    <span class="navi-text">New Group</span>
+                                                </a>
+                                            </li>
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-list-3"></i></span>
+                                                    <span class="navi-text">Contacts</span>
+                                                </a>
+                                            </li>
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-rocket-1"></i></span>
+                                                    <span class="navi-text">Groups</span>
+                                                    <span class="navi-link-badge">
+                                                        <span
+                                                            class="label label-light-primary label-inline font-weight-bold">new</span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-bell-2"></i></span>
+                                                    <span class="navi-text">Calls</span>
+                                                </a>
+                                            </li>
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-gear"></i></span>
+                                                    <span class="navi-text">Settings</span>
+                                                </a>
+                                            </li>
+
+                                            <li class="navi-separator my-3"></li>
+
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i
+                                                            class="flaticon2-magnifier-tool"></i></span>
+                                                    <span class="navi-text">Help</span>
+                                                </a>
+                                            </li>
+                                            <li class="navi-item">
+                                                <a href="#" class="navi-link">
+                                                    <span class="navi-icon"><i class="flaticon2-bell-2"></i></span>
+                                                    <span class="navi-text">Privacy</span>
+                                                    <span class="navi-link-badge">
+                                                        <span
+                                                            class="label label-light-danger label-rounded font-weight-bold">5</span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                        <!--end::Navigation-->
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end::Toolbar-->
+                            <!--begin::User-->
+                            <div class="d-flex align-items-center mb-7">
+                                <!--begin::Pic-->
+                                <div class="flex-shrink-0 mr-4 mt-lg-0 mt-3">
+                                    <div class="symbol symbol-circle symbol-lg-75">
+                                        <img src="assets/media/users/default.jpg" alt="image" />
+                                    </div>
+                                    <div class="symbol symbol-lg-75 symbol-circle symbol-primary d-none">
+                                        <span class="font-size-h3 font-weight-boldest">JM</span>
+                                    </div>
+                                </div>
+                                <!--end::Pic-->
+                                <!--begin::Title-->
+                                <div class="d-flex flex-column">
+                                    <a href="#"
+                                        class="text-dark font-weight-bold text-hover-primary font-size-h4 mb-0">Charlie
+                                        Stone</a>
+                                    <span class="text-muted font-weight-bold">Head of Development</span>
+                                </div>
+                                <!--end::Title-->
+                            </div>
+                            <!--end::User-->
+                            <!--begin::Desc-->
+                            <!--end::Desc-->
+                            <!--begin::Info-->
+                            <div class="mb-7">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <span class="text-dark-75 font-weight-bolder mr-2">Email:</span>
+                                    <a href="#" class="text-muted text-hover-primary">luca@festudios.com</a>
+                                </div>
+                                <div class="d-flex justify-content-between align-items-cente my-1">
+                                    <span class="text-dark-75 font-weight-bolder mr-2">Phone:</span>
+                                    <a href="#" class="text-muted text-hover-primary">44(76)34254578</a>
+                                </div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <span class="text-dark-75 font-weight-bolder mr-2">Location:</span>
+                                    <span class="text-muted font-weight-bold">Barcelona</span>
+                                </div>
+                            </div>
+                            <!--end::Info-->
+                            <a href="#"
+                                class="btn btn-block btn-sm btn-light-danger font-weight-bolder text-uppercase py-4">write
+                                message</a>
+                        </div>
+                        <!--end::Body-->
+                    </div>
+                    <!--end::Card-->
+                </div>
+                <!--end::Col-->
+            </div>
+            <!--end::Row-->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Entry-->
+</div>
+<!--end::Content-->
--- kld-events/admin/partials/_aside.php
+++ kld-events/admin/partials/_aside.php
@@ -215,7 +215,7 @@
 					<div class="menu-submenu "><i class="menu-arrow"></i>
 						<ul class="menu-subnav">
 							<li class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-								<a href="#" class="menu-link"><i
+								<a href="?page=organizer" class="menu-link"><i
 										class="menu-bullet menu-bullet-dot"><span></span></i><span
 										class="menu-text">View Organizers</span></a>
 							</li>
